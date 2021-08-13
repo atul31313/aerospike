5a1adc375fd7ddc9ceeea02d02b2141b275ad097
atul31313 <atul31313>
2021-08-13 06:46:40 +0000

    Initial commit

---
+++ src/test/java/com/idfcbank/poc/AerospikeTest.java
@@ -0,0 +1,21 @@
+package com.idfcbank.poc;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class AerospikeTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
